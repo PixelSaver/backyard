abbc186ca3aef9a7abfc12f6aba3aacf49b147e1
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2025-03-15 18:29:32 -0700

    GOt some stuff working

--- Scenes/double_click_next.gd
+++ Scenes/double_click_next.gd
@@ -3,14 +3,21 @@ extends Button
 signal button_two_down
 
 var clicked_once = false
-@onready var rich_text = $RichTextLabel
+@onready var rich_text = $Yes
+@onready var not_yet_text = $No
 func _ready():
 	connect("button_down", Callable(self, "_on_clicked"))
 	
 func _on_clicked():
 	print("single")
 	if clicked_once == true:
-		button_two_down.emit()
+		if true:#Global.pass_col_one():
+			button_two_down.emit()
+		#else:
+			#not_yet_text.visible = true
+			#await get_tree().create_timer(3).timeout
+			#not_yet_text.visible = false
+			
 	else: 
 		clicked_once = true
 		toggle_double_click_text(clicked_once)
--- Scripts/global.gd
+++ Scripts/global.gd
@@ -3,13 +3,16 @@ extends Node2D
 var curr_col : int = 0
 
 signal traverse_form 
-
-func pass_col_one() -> bool:
-	if $"Lists/1/OptionButton".text != "Backyard Silicon Valley":
-		return false
-	elif $"Lists/1/EmailInput".text == "":
-		return false
-	elif $"Lists/1/Control/First".text != "First Name" and $"Lists/1/Control/Last".text != "Last Name":
-		return false
-	#elif 
-	return true
+#
+#func pass_col_one() -> bool:
+	##if $"Lists/1/OptionButton".get_selected_id() == 1:
+		##return false
+	#if $"Lists/1/EmailInput".text == "":
+		#return false
+	#elif $"Lists/1/Control/First".text != "First Name" and $"Lists/1/Control/Last".text != "Last Name":
+		#return false
+	#elif $"Lists/1/SliderSet/HSlider".value == 0:
+		#return false
+	#elif $"Lists/1/Control4/EmailInput".text == "":
+		#return false
+	#return true
